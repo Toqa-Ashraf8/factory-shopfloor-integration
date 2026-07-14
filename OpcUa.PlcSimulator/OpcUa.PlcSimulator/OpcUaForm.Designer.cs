80355572378901c8671aed3bdfe6e22dfd9224a6
Toqa Ashraf <toqaaashraf87@gmail.com>
2026-07-14 23:42:52 +0300

    implement OPC UA client dashboard to read real-time PLC simulation data

---
+++ OpcUa.PlcSimulator/OpcUa.PlcSimulator/OpcUaForm.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace OpcUa.PlcSimulator
+{
+    partial class OpcUaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtOpcUrl = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblTemperature = new System.Windows.Forms.Label();
+            this.opcTimer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtOpcUrl
+            // 
+            this.txtOpcUrl.Location = new System.Drawing.Point(116, 261);
+            this.txtOpcUrl.Name = "txtOpcUrl";
+            this.txtOpcUrl.Size = new System.Drawing.Size(540, 22);
+            this.txtOpcUrl.TabIndex = 0;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConnect.Font = new System.Drawing.Font("Segoe Print", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.ForeColor = System.Drawing.Color.White;
+            this.btnConnect.Location = new System.Drawing.Point(188, 317);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(171, 50);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.BackColor = System.Drawing.Color.Firebrick;
+            this.btnDisconnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDisconnect.Font = new System.Drawing.Font("Segoe Print", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisconnect.ForeColor = System.Drawing.Color.White;
+            this.btnDisconnect.Location = new System.Drawing.Point(400, 317);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(187, 50);
+            this.btnDisconnect.TabIndex = 2;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = false;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Segoe UI", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(74, 546);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(117, 45);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Status";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.groupBox1.Controls.Add(this.lblTemperature);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.groupBox1.Location = new System.Drawing.Point(692, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(402, 388);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PLC Temperature Reading";
+            // 
+            // lblTemperature
+            // 
+            this.lblTemperature.AutoSize = true;
+            this.lblTemperature.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemperature.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTemperature.Location = new System.Drawing.Point(78, 151);
+            this.lblTemperature.Name = "lblTemperature";
+            this.lblTemperature.Size = new System.Drawing.Size(70, 81);
+            this.lblTemperature.TabIndex = 0;
+            this.lblTemperature.Text = "0";
+            // 
+            // opcTimer
+            // 
+            this.opcTimer.Interval = 1000;
+            this.opcTimer.Tick += new System.EventHandler(this.opcTimer_Tick);
+            // 
+            // OpcUaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1236, 671);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtOpcUrl);
+            this.Name = "OpcUaForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OPC UA Live Dashboard";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOpcUrl;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblTemperature;
+        private System.Windows.Forms.Timer opcTimer;
+    }
+}
+
---
+++ OpcUa.PlcSimulator/OpcUa.PlcSimulator/OpcUaForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows.Forms;
+using Opc.UaFx.Client;
+using System.Collections.Generic;
+using Opc.UaFx;
+
+namespace OpcUa.PlcSimulator
+{
+    public partial class OpcUaForm : Form
+    {
+        private OpcClient opcClient;
+        public OpcUaForm()
+        {
+            InitializeComponent();
+            txtOpcUrl.Text = "opc.tcp://DESKTOP-5RMLPFJ:53530/OPCUA/SimulationServer";
+            lblStatus.Text = "Status: Disconnected";
+            lblStatus.ForeColor = System.Drawing.Color.Red;
+        }
+
+        
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                opcClient = new OpcClient(txtOpcUrl.Text);
+                opcClient.Connect();
+                lblStatus.Text = "Status: Connected to OPC UA!";
+                lblStatus.ForeColor = System.Drawing.Color.Green;
+                opcTimer.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Connection failed: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void opcTimer_Tick(object sender, EventArgs e)
+        {
+            if (opcClient != null && opcClient.State == OpcClientState.Connected)
+            {
+                try
+                {
+                    string constantNodeId = "ns=3;i=1007"; 
+                    string counterNodeId = "ns=3;i=1001";
+                    string randomNodeId = "ns=3;i=1002";
+                    string sawtoothNodeId = "ns=3;i=1003";
+                    string sinusoidNodeId = "ns=3;i=1004";
+                    string squareNodeId = "ns=3;i=1005";
+                    string triangleNodeId = "ns=3;i=1006";
+
+                    IEnumerable<OpcValue> myNodesCollection = opcClient.ReadNodes(new OpcNodeId[] 
+                    { 
+                        constantNodeId, 
+                        counterNodeId, 
+                        randomNodeId, 
+                        sawtoothNodeId, 
+                        sinusoidNodeId, 
+                        squareNodeId, 
+                        triangleNodeId 
+                    });
+                    var valuesList = System.Linq.Enumerable.ToList(myNodesCollection);
+                    
+                    if (valuesList.Count > 0 && valuesList[1] != null && valuesList[1].Value != null)
+                    {
+                        lblTemperature.Text = valuesList[1].Value.ToString() + " °C";
+                    }
+                    
+                }
+                catch (Exception ex)
+                {
+                    opcTimer.Stop();
+                    MessageBox.Show("Error reading nodes: " + ex.Message, "OPC Read Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    lblStatus.Text = "Status: Error Reading!";
+                    lblStatus.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (opcClient != null)
+            {
+                opcTimer.Stop();
+                opcClient.Disconnect();
+                lblStatus.Text = "Status: Disconnected";
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+    }
+}
